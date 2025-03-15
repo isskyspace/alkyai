fd9faf8899d5a1144bb293c301e8fd537af1a1aa
isskyspace <129604117+isskyspace@users.noreply.github.com>
2025-03-15 22:17:31 +0100

    Add files via upload

--- admission.php
+++ admission.php
@@ -1,17 +1,17 @@
 <?php
 // Informations de connexion à la base de données PostgreSQL
-$host = "dpg-cv8aij8gph6c73brekk0-a";   // Remplacez par l'hôte de votre base de données PostgreSQL sur Render
-$dbname = "AlkyAI";     // Nom de votre base de données
-$username = "root";  // Nom d'utilisateur PostgreSQL
+$host = "dpg-cv8aij8gph6c73brekk0-a"; // Remplacez par l'hôte de votre base de données PostgreSQL sur Render
+$dbname = "AlkyAI"; // Nom de votre base de données
+$username = "root"; // Nom d'utilisateur PostgreSQL
 $password = "EbbN7NlSbZnnfYk9whvIn8smLIXCQoj8"; // Mot de passe PostgreSQL
-$port = "5432";         // Port par défaut pour PostgreSQL
+$port = "5432"; // Port par défaut pour PostgreSQL
 
 // Création de la connexion
 $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$username;password=$password";
 
 try {
     $conn = new PDO($dsn);
-    // Définir le mode d'erreur PDO pour les exceptions
+    // Définit le mode d'erreur PDO pour les exceptions
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connexion réussie !";
 } catch (PDOException $e) {
@@ -30,11 +30,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $apport = $_POST['apport'];
 
     // Préparation de la requête SQL pour insérer les données dans la table
-    $query = "INSERT INTO adhesion (nom, prenom, email, tel, adresse, apport) 
-              VALUES (:nom, :prenom, :email, :tel, :adresse, :apport)";
-
+    $query = "INSERT INTO adhésion (nom, prenom, email, tel, adresse, apport) VALUES (:nom, :prenom, :email, :tel, :adresse, :apport)";
     $stmt = $conn->prepare($query);
-    // Lier les paramètres pour éviter les injections SQL
+
+    // Liaison des paramètres pour éviter les injections SQL
     $stmt->bindParam(':nom', $nom);
     $stmt->bindParam(':prenom', $prenom);
     $stmt->bindParam(':email', $email);
@@ -42,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->bindParam(':adresse', $adresse);
     $stmt->bindParam(':apport', $apport);
 
-    // Exécuter la requête
+    // Exécution de la requête
     if ($stmt->execute()) {
         echo "Données enregistrées avec succès !";
     } else {
